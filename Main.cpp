b89ace87f7d67437d36c13557c81c90ab4396a51
GH Desktop <144569041+oabdulr@users.noreply.github.com>
2024-03-11 16:22:18 -0400

    Ok Actually fixed troll

--- Main.cpp
+++ Main.cpp
@@ -1,42 +1,28 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
-int main(int argc, char** argv)
+int main(void)
 {
+    GLFWwindow* window;
 
-    glfwInit();
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    if (!glfwInit())
+        return -1;
 
-
-    GLFWwindow* window = glfwCreateWindow(1280, 720, "Black Screen", NULL, NULL);
-
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
 
     glfwMakeContextCurrent(window);
 
-    gladLoadGL();
-
-    glViewport(0, 0, 1280, 720);
-
-    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-    glClear(GL_COLOR_BUFFER_BIT);
-    glfwSwapBuffers(window);
-
     while (!glfwWindowShouldClose(window))
     {
+        glClear(GL_COLOR_BUFFER_BIT);
+        glfwSwapBuffers(window);
         glfwPollEvents();
     }
 
-    glfwDestroyWindow(window);
     glfwTerminate();
-
-
     return 0;
-
- 
 }
-
-
